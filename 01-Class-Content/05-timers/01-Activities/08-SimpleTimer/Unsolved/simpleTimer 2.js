677fae2dfd478243e7f5ba54e93e940696c85e5a
John Carlo <carlobase@gmail.com>
2020-01-11 14:05:49 -0600

    Copied all files in copy repo

---
+++ 01-Class-Content/05-timers/01-Activities/08-SimpleTimer/Unsolved/simpleTimer 2.js
@@ -0,0 +1,38 @@
+// Step 1:
+// Use the following Audio file below:
+
+var audio = new Audio("raven.mp3");
+
+//  Step 2: do the following between the --- comments below
+//  after 5 seconds, execute the fiveSeconds function
+//  after 10 seconds, execute the tenSeconds function
+//  after 15 seconds, execute the timeUp function
+
+setTimeout(fiveSeconds,1000 *5);
+setTimeout(tenSeconds, 1000 *10);
+setTimeout(timeup, 1000 *15);
+
+
+// Step 3:
+// Fill in the blanks to these functions.
+function fiveSeconds() {
+  // in the element with an id of `time-left` add an h2 saying About 10 Seconds Left!
+  // console log 10 seconds left
+  $("#time-left").append("<h2>10 seconds left!</h2>");
+  console.log("10 secs left");
+}
+
+function tenSeconds() {
+  // in the element with an id of `time-left` add an h2 saying About 5 Seconds Left!
+  // console log 5 seconds left
+  $("#time-left").append("<h2>5 seconds left!</h2>");
+}
+
+function timeUp() {
+  // in the element with an id of `time-left` add an h2 saying Time's Up!
+  // console log done
+  $("#time-up").append("<h2>Time Ups</h2>");
+
+  // The following line will play the audio file above
+  //audio.play();
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # COPY_UT_Class_Activities
+
+
+Updated:  01/11/2020
